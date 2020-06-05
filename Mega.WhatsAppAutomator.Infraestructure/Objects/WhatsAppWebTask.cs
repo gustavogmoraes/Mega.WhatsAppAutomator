4bb35e698fe669b69c1adf973809cbb2a4abb320
Gustavo Moraes <gustavog.moraes@outlook.com>
2020-06-04 23:24:27 -0300

    Now really working

--- Mega.WhatsAppAutomator.Infraestructure/Objects/WhatsAppWebTask.cs
+++
@@ -1,9 +0,0 @@
-namespace Mega.WhatsAppAutomator.Infraestructure.Objects
-{
-    public class WhatsAppWebTask
-    {
-        public string KindOfTask { get; set; }
-
-        public object TaskData  { get; set; }
-    }
-}
